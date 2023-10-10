6fa720ba65f12a2a5db657b9deb91cfbdb9b78ea
jjaljuria <josejavieral13@gmail.com>
2023-10-09 21:33:19 -0400

    added eslint

--- index.js
+++ index.js
@@ -1,67 +1,62 @@
-const express = require('express');
-const app = express();
-const socketIo = require('socket.io');
-const path = require('path');
-const { engine } = require('express-handlebars');
-const cookieParser = require('cookie-parser');
+const express = require('express')
+const app = express()
+const socketIo = require('socket.io')
+const path = require('path')
+const { engine } = require('express-handlebars')
+const cookieParser = require('cookie-parser')
 
 app.engine('handlebars', engine({
-	defaultLayout: false
+  defaultLayout: false
 }))
 app.set('view engine', 'handlebars')
 app.set('views', './views')
 
-
-app.set('PORT', process.env.PORT || 3000);
+app.set('PORT', process.env.PORT || 3000)
 
 /** STATICS */
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
-
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
 
 /** MIDDLEWARES */
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cookieParser('secret'))
 
-app.get('/', (req, res)	=>{
-	const nickname = req.cookies['chat_nickname'];
-	
-	if(!nickname) return res.redirect('/login')
+app.get('/', (req, res) => {
+  const nickname = req.cookies.chat_nickname
 
-	res.render('index.handlebars', {nickname});
-});
+  if (!nickname) return res.redirect('/login')
 
-app.get('/login', (req, res) =>{
-	res.render('login.handlebars');
-});
-
-app.post('/login', (req, res) =>{
-	const {nickname} = req.body;
-	
-
-	if(!nickname) return res.status(400).send('error en el nickname')
-	
-
-	return res.cookie('chat_nickname', String(nickname), {
-		maxAge: 1000 * 60 * 60 * 24 * 5,
-		sameSite: 'strict'
-	}).redirect('/')
+  res.render('index.handlebars', { nickname })
 })
 
-app.get('/logout', (req,res) =>{
-	return res.clearCookie('chat_nickname').redirect('/login')
+app.get('/login', (req, res) => {
+  res.render('login.handlebars')
 })
 
+app.post('/login', (req, res) => {
+  const { nickname } = req.body
 
-const server = app.listen(app.get('PORT'), () =>{
-	console.log('Server start in PORT ' + app.get('PORT'));
-});
+  if (!nickname) return res.status(400).send('error en el nickname')
+
+  return res.cookie('chat_nickname', String(nickname), {
+    maxAge: 1000 * 60 * 60 * 24 * 5,
+    sameSite: 'strict'
+  }).redirect('/')
+})
+
+app.get('/logout', (req, res) => {
+  return res.clearCookie('chat_nickname').redirect('/login')
+})
+
+const server = app.listen(app.get('PORT'), () => {
+  console.log('Server start in PORT ' + app.get('PORT'))
+})
 
 const io = socketIo(server)
-io.on('connection', (socket)=>{
-	console.log('Contected in ' + socket.id);
-	socket.on('chat', (data)=>{
-		io.sockets.emit('chat', data);
-	})
+io.on('connection', (socket) => {
+  console.log('Contected in ' + socket.id)
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data)
+  })
 })
--- public/chat.js
+++ public/chat.js
@@ -1,27 +1,26 @@
-const socket = io();
+const socket = io()
 
-const send = document.getElementById('send');
-const text = document.getElementById('text');
-const conversations = document.getElementById('conversations');
+const send = document.getElementById('send')
+const text = document.getElementById('text')
+const conversations = document.getElementById('conversations')
 const nicknameFile = document.getElementById('nickname')
 
-send.addEventListener('click', ()=>{
-
-	socket.emit('chat', {
-		userId: socket.id, 
-		text: text.value,
-		nickname: nicknameFile.value
-	});
-	text.value = '';
+send.addEventListener('click', () => {
+  socket.emit('chat', {
+    userId: socket.id,
+    text: text.value,
+    nickname: nicknameFile.value
+  })
+  text.value = ''
 })
 
-socket.on('chat', ({userId, text, nickname})=>{
-	let username = 'you';
-	if(userId !== socket.id){
-		username = nickname
-	}
+socket.on('chat', ({ userId, text, nickname }) => {
+  let username = 'you'
+  if (userId !== socket.id) {
+    username = nickname
+  }
 
-	conversations.innerHTML = conversations.innerHTML + `<span class="d-block mb-1"><b>${username}</b>: ${text}</span>`;
+  conversations.innerHTML = conversations.innerHTML + `<span class="d-block mb-1"><b>${username}</b>: ${text}</span>`
 
-	conversations.scrollTop = conversations.scrollHeight;
+  conversations.scrollTop = conversations.scrollHeight
 })
