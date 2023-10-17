d57fcadb35babc9627a3f2f66a5028996b9a3cd8
jjaljuria <josejavieral13@gmail.com>
2023-10-17 13:24:02 -0400

    made dotenv

---
+++ config.js
@@ -0,0 +1,9 @@
+const { config } = require('dotenv')
+
+config()
+
+module.exports = {
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_USER: process.env.DB_USER || 'root',
+  DB_PASSWORD: process.env.DB_PASSWORD || ''
+}
