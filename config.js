05c46127c064817bae2e3d9babc4dd746d14c1b6
jjaljuria <josejavieral13@gmail.com>
2023-10-20 20:58:50 -0400

    created db

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ config()
 module.exports = {
   DB_HOST: process.env.DB_HOST || 'localhost',
   DB_USER: process.env.DB_USER || 'root',
-  DB_PASSWORD: process.env.DB_PASSWORD || ''
+  DB_PASSWORD: process.env.DB_PASSWORD || '',
+  DB_NAME: process.env.DB_NAME || 'chat_cat'
 }
---
+++ models/database.js
@@ -0,0 +1,12 @@
+const mariadb = require('mariadb')
+const config = require('../config')
+
+const pool = mariadb.createPool({
+  host: config.DB_HOST,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: config.DB_NAME,
+  connectionLimit: 5
+})
+
+module.exports = { pool }
