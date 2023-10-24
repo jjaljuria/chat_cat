efe9d5437c88e90664ac315d6d24439ae064c601
jjaljuria <josejavieral13@gmail.com>
2023-10-24 10:59:53 -0400

    made encrypt password

--- routers/user.routes.js
+++ routers/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { prisma } from '../database.js'
+import encrypt from '../lib/encrypt.js'
 
 const router = Router()
 
@@ -9,9 +10,10 @@ router.get('/user', async (req, res) => {
 
 router.post('/user', async (req, res) => {
   try {
-    await prisma.user.create({ data: req.body })
+    await prisma.user.create({ data: { ...req.body, password: await encrypt(req.body.password) } })
     res.status(202).send()
   } catch (error) {
+    console.trace(error)
     res.status(500).render('500.handlebars', { message: 'Error with create user' })
   }
 })
