f46ad937cadbe3166d394fae5cf3b76c43d56f68
jjaljuria <josejavieral13@gmail.com>
2024-01-01 09:27:54 -0400

    conecto to

--- client/src/components/ItemUser.jsx
+++ client/src/components/ItemUser.jsx
@@ -3,15 +3,25 @@ import * as ConversationServices from '../services/ConversationServices';
 import { useConversations } from '../store/ConversationStore';
 
 export default function ItemUser({user}) {
-    const setCurrentConversation = useConversations(state => state.setCurrentConversation)
+    const { setCurrentConversation, setConversations } = useConversations(state => ({
+        setCurrentConversation: state.setCurrentConversation,
+        setConversations: state.setConversations
+    }))
+
+    const conversations = useConversations(state => state.conversations)
 
     const connectTo = async (idUser) =>{
-        setCurrentConversation(await ConversationServices.create(idUser))
+        const newConversation = await ConversationServices.create(idUser)
+        setConversations([...new Set(conversations, newConversation)])
+        setCurrentConversation(newConversation)
     }
 
     return (
         <li className="list-group-item list-group-item-action p-0">
-            <a className="text-reset text-decoration-none py-2 px-3 d-block" draggable="false" onClick={()=> connectTo(user.id)} >
+            <a className="text-reset text-decoration-none py-2 px-3 d-block" draggable="false" onClick={(e)=> {
+                e.preventDefault()
+                connectTo(user.id)
+            }} >
                 {user.nickname}
             </a>
         </li>
